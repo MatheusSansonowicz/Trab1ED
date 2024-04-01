1c2e55c6e6497a091e997b454585676ff9f30ee9
MatheusSansonowicz <128191037+MatheusSansonowicz@users.noreply.github.com>
2024-04-01 19:23:11 -0300

    Update OutroMétodo.java

--- OutroMétodo.java
+++ OutroMétodo.java
@@ -12,6 +12,35 @@ public class OutroMétodo {
             System.out.println(item);
         }
     }
+
+	public class GerenciadorNumeros {
+
+	    public static List<Numero> contarAparicoes(List<List<Integer>> listaGenerica) {
+	        List<Numero> numeros = new ArrayList<>();
+
+	        for (List<Integer> listaInterna : listaGenerica) {
+	            for (Integer valor : listaInterna) {
+	                Numero numero = encontrarNumero(numeros, valor);
+	                if (numero == null) {
+	                    numeros.add(new Numero(valor));
+	                } else {
+	                    numero.incrementarAparicoes();
+	                }
+	            }
+	        }
+
+	        return numeros;
+	    }
+
+	    private static Numero encontrarNumero(List<Numero> numeros, int valor) {
+	        for (Numero numero : numeros) {
+	            if (numero.getValor() == valor) {
+	                return numero;
+	            }
+	        }
+	        return null;
+	    }
+	}
 	
 	public static void exibirValoresEmTodasListas(List<List<Integer>> listaGenerica) {
 	    List<Numero> numeros = GerenciadorNumeros.contarAparicoes(listaGenerica);
@@ -40,7 +69,7 @@ public class OutroMétodo {
 
 	public static void main(String[] args) {
 		
-		List<Integer> lista = new ArrayList<Integer>();
+		//List<Integer> lista = new ArrayList<Integer>();
         Random gerador = new Random();
         
         
@@ -49,7 +78,7 @@ public class OutroMétodo {
         for (int i = 0; i < 5; i++) {
             List<Integer> listaInterna = new ArrayList<>();
             for (int j = 0; j < 10; j++) {
-                int valor = gerador.nextInt(400);
+                int valor = gerador.nextInt(40);
                 if (!listaInterna.contains(valor)) { // Verifique se o valor já está na lista interna
                     listaInterna.add(valor); // Adicione à lista interna
                 }
@@ -58,8 +87,11 @@ public class OutroMétodo {
             listaGenerica.add(listaInterna); // Adicione a lista interna à lista genérica
         }
         // Exiba todas as listas internas da lista genérica
+        int nlista = 1;
         for (List<Integer> listaInterna : listaGenerica) {
+        	System.out.println("Exibindo a lista "+nlista);
             exibir(listaInterna);
+            nlista++;
         }
         
      // Exibir valores presentes em todas as listas
